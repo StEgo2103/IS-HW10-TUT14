d48dea722d2295ae22e34ba0d800356c4b4e473b
StEgo2103 <luca@deltort.net>
2024-11-18 15:27:59 -0800

    Add project

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const { Pool } = require('pg');
+const multer = require('multer');
+const upload = multer();
+
 require('dotenv').config();
 
 const app = express();
@@ -33,5 +36,56 @@ app.get('/', (req, res) => {
 			message: message,
 			model: model,
 		});
+	});
+});
+
+app.get('/input', (req, res) => {
+	res.render('input');
+});
+app.post('/input', upload.single('filename'), (req, res) => {
+	if (!req.file || Object.keys(req.file).length === 0) {
+		let message = 'Error: Import file not uploaded';
+		return res.send(message);
+	}
+	//Read file line by line, inserting records
+	const buffer = req.file.buffer;
+	const lines = buffer.toString().split(/\r?\n/);
+	lines.forEach((line) => {
+		//console.log(line);
+		let product = line.split(',');
+		//console.log(product);
+		const sql = 'INSERT INTO PRODUCT(prod_id, prod_name, prod_desc, prod_price) VALUES ($1, $2, $3, $4)';
+		pool.query(sql, product, (err, result) => {
+			if (err) {
+				console.log(`Insert Error. Error message: ${err.message}`);
+			} else {
+				console.log(`Inserted successfully`);
+			}
+		});
+	});
+	let message = `Processing Complete - Processed ${lines.length} records`;
+	res.send(message);
+});
+
+app.get('/output', (req, res) => {
+	let message = '';
+	res.render('output', { message: message });
+});
+app.post('/output', (req, res) => {
+	const sql = 'SELECT * FROM PRODUCT ORDER BY PROD_ID';
+	pool.query(sql, [], (err, result) => {
+		let message = '';
+		if (err) {
+			message = `Error - ${err.message}`;
+			res.render('output', { message: message });
+		} else {
+			let output = '';
+			result.rows.forEach((product) => {
+				output += `${product.prod_id},${product.prod_name},${product.prod_desc},${product.prod_price}\r\n`;
+			});
+			res.header('Content-Type', 'text/csv');
+			res.attachment('export.csv');
+			return res.send(output);
+		}
 	});
 });
